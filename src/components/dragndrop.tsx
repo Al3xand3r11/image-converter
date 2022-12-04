c768381b339e76638db8ea97c7b8204764df91c7
Al3xand3r11 <alexander.nance11@gmail.com>
2022-12-03 20:49:32 -0500

    test

--- src/components/dragndrop.tsx
+++ src/components/dragndrop.tsx
@@ -1,13 +1,21 @@
-import  React from 'react'
+import  React, { useCallback } from 'react'
 import Picture from '../images/filepic.png'
 import {useDropzone} from 'react-dropzone'
 
 
+
 const Dragndrop = () => {
-    const {getRootProps, getInputProps} = useDropzone({});
+
+   const onDrop = useCallback((acceptedFiles: any) => {console.log(acceptedFiles)
+  }, []);
+
+    const {getRootProps, getInputProps} = useDropzone({
+      onDrop,
+      multiple: false, 
+    });
   return (
-    <div>
-    <div {...getRootProps()} className = "test">
+    <div className = "flex flex-col items-center justify-center h-full space-y-3 border-2 border-dashed">
+    <div {...getRootProps()} className = "w-full h-80 rounded-md cursor-pointer focus:outline-none">
         <input{...getInputProps()}/>
         <p>
         <img src={Picture} alt=""/>
